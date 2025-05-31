d98bb12e665c33a4dd10020b27528f20a845ca08
Kamran473 <kamranmanzoor473@gmail.com>
2025-05-31 15:36:56 +0500

    add new code line

--- python code examples/01_opertors.py
+++ python code examples/01_opertors.py
@@ -1,9 +1,12 @@
 #  operators
-# print(2+3)
-# print(5-3)
-# print(4*3)
-# print(4/2)
-# print(4%3)
-# print(4//2)
-# print(4**3)
+print(2+3)
+print(5-3)
+print(4*3)
+print(4/2)
+print(4%3)
+print(4//2)
+print(4**3)
+x = 3 
+y = 9
+print(x+y)
 
